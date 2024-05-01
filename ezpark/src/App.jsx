d8ef1568488a840a00c1773686ec30a36bebe24c
CamiloSanchezCuestas <camisan3005@outlook.com>
2024-04-30 20:59:11 -0500

    Cambios App.jsx

--- ezpark/src/App.jsx
+++ ezpark/src/App.jsx
@@ -3,12 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AddParqueadero from './components/espacio/AddParqueadero'
+import EditParqueadero from './components/espacio/EditParqueadero'
+import ListParqueaderos from './components/espacio/ListParqueaderos'
+import DeleteParqueadero from './components/espacio/DeleteParqueadero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return <>
     <AddParqueadero/>
+    <EditParqueadero/>
+    <DeleteParqueadero/>
+    <ListParqueaderos/>
   </>
 }
 
